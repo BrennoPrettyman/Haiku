1b47408085631af0fdcc627322b4971c7d325f9f
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-17 20:23:56 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Haiku;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("HAIKAI");
+        Console.WriteLine();
+        Console.WriteLine("- Kato Shuson");
+        Console.WriteLine();
+        Console.WriteLine("Eu mato uma formiga");
+        Console.WriteLine("E perceber que meus três filhos");
+        Console.WriteLine("Estavam assistindo.");
+    }
+}
